9d303f336bb4aef360a8df8b9e5894f8456071c9
someFisherman <gloornoe@gmail.com>
2026-01-21 16:44:24 +0100

    Refactor allowedHosts to List and add new host
    Updated allowedHosts to use List instead of Set and added Azure AD B2C Login.

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,14 +1,14 @@
 class AppConfig {
-  // URL lässt sich später via Codemagic überschreiben:
-  // --dart-define=START_URL=https://release-domain.com/
   static const String startUrl = String.fromEnvironment(
     'START_URL',
-    defaultValue: "https://soleco-optimizer-beta.azurewebsites.net/",
+    defaultValue: 'https://soleco-optimizer-beta.azurewebsites.net/',
   );
 
-  // Domain-Lock: nur diese Hosts werden innerhalb der App geladen
-  static const allowedHosts = <String>{
-    "soleco-optimizer-beta.azurewebsites.net",
-    // später z.B. "soleco-optimizer.com"
-  };
+  static const List<String> allowedHosts = [
+    // Deine Website
+    'soleco-optimizer-beta.azurewebsites.net',
+
+    // Azure AD B2C Login (dein Redirect)
+    'crystalball.b2clogin.com',
+  ];
 }
